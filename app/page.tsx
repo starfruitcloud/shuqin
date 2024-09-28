f026c43438e3295e2122e4adacb9715d40a79868
dezhizhang <1541609448@qq.com>
2024-09-28 23:31:12 +0800

    feat: 请求后端数据

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,22 @@
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-09-28 20:34:17
  * :last editor: 张德志
- * :date last edited: 2024-09-28 21:05:07
+ * :date last edited: 2024-09-28 23:21:31
  */
+"use client"
 
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "数擎科技",
-};
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <div>数擎科技</div>;
+  const [data,setData] = useState([])
+  const featchTopics = async () => {
+    const baseUrl = 'https://cnodejs.org/api/v1'
+    const rsp = await fetch(`${baseUrl}/topics`).then((rsp) => rsp.json());
+    setData(rsp.data)
+  
+  };
+  useEffect(() => {
+    featchTopics();
+  }, []);
+  return <div>{data.map((item) => <h3 key={item._id}>{item.title}</h3>)}</div>;
 }
-
